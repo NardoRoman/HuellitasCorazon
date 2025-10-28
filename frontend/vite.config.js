10c21ed50dffbc3e61296845c5a064dc468493c9
BERNARDO\nardo <narditchnoviembre02@gmail.com>
2025-10-28 16:27:46 -0600

    fix: netlify configuration with base ./ and spa redirects

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,12 +2,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: './',
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src') // Definimos el alias '@' para que apunte a la carpeta 'src'
+      '@': path.resolve(__dirname, './src')
     }
   }
 })

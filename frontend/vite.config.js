34b269c30a0fb583f6362acab8c871aa246e54da
NardoRoman <125240603+NardoRoman@users.noreply.github.com>
2025-10-27 22:28:22 -0600

    Set base path for Vite configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '', // ← CAMBIA a CADENA VACÍA
+  base: '/HuellitasCorazon/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')

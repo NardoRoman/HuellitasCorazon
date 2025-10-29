ae862951944875e78b6f6916810d81e0612b7b79
BERNARDO\nardo <narditchnoviembre02@gmail.com>
2025-10-28 21:23:33 -0600

    fix: clean vite.config.js merge conflicts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,18 +4,10 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-<<<<<<< HEAD
   base: './',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
-=======
-  base: '/HuellitasCorazon/',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '/img': path.resolve(__dirname, './public/img') // ← NUEVO ALIAS
->>>>>>> f5c0b2a83b7502eccbbbaec3e085d1747572ba8d
     }
   }
 })
